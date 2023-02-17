f070aa6dfe7086e0189af0349ba7230f7102ea4f
yana-glt <galitsynayasya@gmail.com>
2023-02-17 15:42:37 +0100

    create project, add logger

---
+++ src/main/java/com/solvd/scheduler/Main.java
@@ -0,0 +1,12 @@
+package com.solvd.scheduler;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private final static Logger logger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Logger verification");
+    }
+}
