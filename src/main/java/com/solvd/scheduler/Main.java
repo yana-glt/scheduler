f40e543dd95024018c461fa4e2a9537e3935c507
asiajanicka <asia.janicka@gmil.com>
2023-02-22 00:02:39 +0100

    refactor: post rebase changes

--- src/main/java/com/solvd/scheduler/Main.java
+++ src/main/java/com/solvd/scheduler/Main.java
@@ -11,24 +11,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.List;
 import java.util.stream.Collectors;
 
-<<<<<<< HEAD
-public class Main{
-
-    private final static Logger logger = LogManager.getLogger(Main.class.getName());
-=======
 public class Main {
     private final static Logger logger = LogManager.getLogger(Main.class);
->>>>>>> ac2d2d1 (Added logger messages)
     private static final int NUM_DAYS = 5;
     private static final int MIN_LESSONS = 2;
     private static final int MAX_LESSONS = 8;
 
-<<<<<<< HEAD
     public static void main(String[] args){
         logger.info("Check logger");
-=======
-    public static void main(String[] args) {
->>>>>>> ac2d2d1 (Added logger messages)
         IGroupService groupService = new GroupService();
         List<Group> groupsAndTheirSubjectWithTimePerWeek = groupService.groupsAndTheirSubjectWithTimePerWeek();
         for (Group g : groupsAndTheirSubjectWithTimePerWeek) {
